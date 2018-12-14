c73685888ccd8c8b2ad504d82fcc2e27b4e553a0
Fred Glass <glasshcs@gmail.com>
2018-12-14 17:29:07 +0000

    Day 1

--- src/main/java/glass/fred/advent2018/Day1.java
+++ src/main/java/glass/fred/advent2018/Day1.java
@@ -1,4 +1,40 @@
 package main.java.glass.fred.advent2018;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Day1 {
+
+    public void run(String filename, boolean part1) {
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+
+            if (part1) {
+                int frequency = stream.mapToInt(Integer::valueOf).sum();
+                System.out.println("Total frequency: " + frequency);
+                return;
+            }
+
+            int frequency = 0;
+            List<Integer> cache = new ArrayList<>();
+            List<Integer> frequencies = stream.map(Integer::valueOf).collect(Collectors.toList());
+
+            while (true) {
+                for (int freq : frequencies) {
+                    frequency += freq;
+                    if (cache.contains(frequency)) {
+                        System.out.println("Frequency reached twice: " + frequency);
+                        return;
+                    } else cache.add(frequency);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
